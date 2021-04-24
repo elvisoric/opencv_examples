1981597b8444f2c777551d898667a363d917abab
Elvis Oric <elvis.oric90@gmail.com>
2021-04-24 21:38:17 +0200

    Initial configuration of project with target to check build.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Computer Vision Intro
+## Introduction to Computer Vision
+
+### Prerequisites
+
+ - conan package manager
+ - meson build system
+ 
+ ### Build
+ 
+ - `mkdir build && cd build`
+ - `conan install ..`
+ - `conan build ..`
---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile, tools, Meson
+import os
+
+class ConanFileToolsTest(ConanFile):
+    generators = "pkg_config"
+    requires = ["fmt/7.0.3", "opencv/4.5.2"]
+    settings = "os", "compiler", "build_type"
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
