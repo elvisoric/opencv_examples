c09ab8ab8e7b713c5e52824626e09e677178e469
Elvis Oric <elviso@bicomsystems.com>
2021-04-30 09:45:58 +0200

    Introduce manipulation of pixels and group of pixels

--- part_one/srcs/first.cpp
+++ part_one/srcs/first.cpp
@@ -2,29 +2,17 @@
 #include <iostream>
 int main() {
   cv::Mat image = cv::imread("images/number_zero.jpg");
+  image.at<cv::Vec3b>(0, 0) = cv::Vec3b{99, 99, 99};
+  image(cv::Range(2, 4), cv::Range(4, 9)).setTo(cv::Scalar(99, 99, 99));
   std::cout << image << std::endl;
-  std::cout << biagile::parseType(image.type()) << std::endl;
-
-  std::cout << image.cols << std::endl;
-  std::cout << image.rows << std::endl;
 
   std::cout << "\n\n\n";
 
   auto grayImage = cv::imread("images/number_zero.jpg", cv::IMREAD_GRAYSCALE);
+  std::cout << static_cast<int>(grayImage.at<uchar>(0, 0)) << std::endl;
+  grayImage.at<uchar>(0, 0) = 123;
+  grayImage.at<uchar>(2, 9) = 123;
   std::cout << grayImage << std::endl;
-  std::cout << biagile::parseType(grayImage.type()) << std::endl;
-
-  std::cout << grayImage.cols << std::endl;
-  std::cout << grayImage.rows << std::endl;
-
-  // RGB
-  // OpenCV BGR
-  cv::Mat customImage{5, 7, CV_8UC3, cv::Scalar{20, 100, 45}};
-  std::cout << customImage << std::endl;
-  std::cout << customImage.size() << std::endl;
-
-  auto clone = image.clone();
-  std::cout << clone << std::endl;
 
   return 0;
 }
