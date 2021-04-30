4cc558326c5edc5e58cfc466cbb22446f6f0504e
Elvis Oric <elviso@bicomsystems.com>
2021-04-30 09:55:07 +0200

    Display image

--- part_one/srcs/first.cpp
+++ part_one/srcs/first.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 int main() {
   cv::Mat image = cv::imread("images/number_zero.jpg");
-  image.at<cv::Vec3b>(0, 0) = cv::Vec3b{99, 99, 99};
-  image(cv::Range(2, 4), cv::Range(4, 9)).setTo(cv::Scalar(99, 99, 99));
-  std::cout << image << std::endl;
+  image.at<cv::Vec3b>(0, 0) = cv::Vec3b{0, 0, 250};
+  image(cv::Range(2, 4), cv::Range(4, 9)).setTo(cv::Scalar(0, 0, 250));
 
-  std::cout << "\n\n\n";
+  cv::imshow("Image", image);
 
   auto grayImage = cv::imread("images/number_zero.jpg", cv::IMREAD_GRAYSCALE);
-  std::cout << static_cast<int>(grayImage.at<uchar>(0, 0)) << std::endl;
   grayImage.at<uchar>(0, 0) = 123;
   grayImage.at<uchar>(2, 9) = 123;
-  std::cout << grayImage << std::endl;
+
+  cv::imshow("Image gray", grayImage);
+
+  cv::waitKey(0);
+  cv::destroyAllWindows();
 
   return 0;
 }
