c2b7a1e828b65a05e264dff41832fc1ed38fda6b
Elvis Oric <elviso@bicomsystems.com>
2021-04-30 10:44:17 +0200

    Introduce thresholding

--- part_one/srcs/first.cpp
+++ part_one/srcs/first.cpp
@@ -1,28 +1,17 @@
 #include <util.hpp>
 #include <iostream>
 int main() {
-  cv::VideoCapture cap{"images/video.mp4"};
-  auto width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-  auto height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+  auto image = cv::imread("images/threshold.png", cv::IMREAD_GRAYSCALE);
+  cv::imshow("Image", image);
 
-  cv::VideoWriter out{"out.mp4", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
-                      20, cv::Size(width, height)};
-  cv::Mat frame;
-  while (cap.isOpened()) {
-    cap >> frame;
-    if (frame.empty()) break;
-    cv::imshow("frame", frame);
-    cv::Mat gray;
-    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-    out.write(gray);
-    cv::waitKey(20);
-  }
+  cv::Mat thresh, threshInv;
+  cv::threshold(image, thresh, 120, 255, cv::THRESH_BINARY);
+  cv::imshow("threshold", thresh);
+
+  cv::threshold(image, threshInv, 120, 255, cv::THRESH_BINARY_INV);
+  cv::imshow("threshold inv", threshInv);
 
   cv::waitKey(0);
   cv::destroyAllWindows();
-
-  cap.release();
-  out.release();
-
   return 0;
 }
