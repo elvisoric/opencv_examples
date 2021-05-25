4dd6e61b903e57f39807b363cf5fc92e90aeddeb
Elvis Oric <elvis.oric90@gmail.com>
2021-05-25 09:51:08 +0200

    Start working on green screen

--- part_two/srcs/green.cpp
+++ part_two/srcs/green.cpp
@@ -1,51 +1,25 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main() {
-  auto image = cv::imread("images/split.png");
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cout << "Please provide background as parameter" << std::endl;
+    return -1;
+  }
 
-  cv::imshow("original", image);
+  const auto backgorundSource = argv[1];
+  auto background = cv::imread(backgorundSource);
 
-  cv::Mat hsv;
-  cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+  cv::imshow("Background", background);
 
-  cv::Mat channels[3];
-  cv::split(hsv, channels);
-  cv::imshow("H", channels[0]);
-  cv::imshow("S", channels[1]);
-  cv::imshow("V", channels[2]);
+  auto image = cv::imread("images/green.jpg");
+  cv::imshow("Image", image);
+  auto crop = background(cv::Range(200, image.rows + 200),
+                         cv::Range(200, image.cols + 200));
 
-  auto factor = channels[2].clone();
-  factor.setTo(50);
-  cv::Mat newImage = channels[2] + factor;
-  cv::imshow("newImage", newImage);
+  cv::imshow("Crop", crop);
 
-  channels[2] = newImage;
+  cv::waitKey(0);
 
-  cv::Mat finalImage;
-  cv::merge(channels, 3, finalImage);
-  cv::cvtColor(finalImage, finalImage, cv::COLOR_HSV2BGR);
-  cv::imshow("final", finalImage);
-  cv::waitKey();
-  cv::destroyAllWindows();
   return 0;
 }
-
-// int main(int argc, char** argv) {
-//   if (argc < 2) {
-//     std::cout << "Please provide background as parameter" << std::endl;
-//     return -1;
-//   }
-//
-//   const auto backgorundSource = argv[1];
-//   auto background = cv::imread(backgorundSource);
-//
-//   cv::imshow("Background", background);
-//
-//   auto image = cv::imread("images/green.jpg");
-//   cv::imshow("Image", image);
-//
-//   cv::waitKey(0);
-//
-//   return 0;
-// }
