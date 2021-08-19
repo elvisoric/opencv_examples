8040e7a21b7bd9d3b015e0d1f4f330546e38c9a5
Elvis Oric <elvis.oric90@gmail.com>
2021-08-19 20:42:17 +0200

    Starting point for computer vision part four

---
+++ part_four/srcs/meme.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <dlib/opencv.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+int main() {
+  std::cout << "Computer Vision Part IV" << std::endl;
+  return 0;
+}
