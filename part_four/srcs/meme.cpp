dc079dba72be2732778405e7af8b6f707a9d0fd0
Elvis Oric <elvis.oric90@gmail.com>
2021-08-19 14:46:43 +0200

    thug life final example

--- part_four/srcs/meme.cpp
+++ part_four/srcs/meme.cpp
@@ -27,7 +27,11 @@ void renderFace(cv::Mat& img, const dlib::full_object_detection& landmarks) {
 
 int main() {
   auto faceDetector = dlib::get_frontal_face_detector();
-  auto image = cv::imread("images/family.jpg");
+  auto image = cv::imread("images/musk.jpg");
+  auto glasses = cv::imread("images/tr.png", cv::IMREAD_UNCHANGED);
+  auto cigar = cv::imread("images/cigar.png", cv::IMREAD_UNCHANGED);
+
+  std::cout << "Cigar before: " << cigar.size() << std::endl;
 
   // Convert OpenCV image format to Dlib's image format
   dlib::cv_image<dlib::bgr_pixel> dlibImage(image);
@@ -40,18 +44,85 @@ int main() {
       "data/models/shape_predictor_68_face_landmarks.dat";
   dlib::deserialize(predictorPath) >> landmarkDetector;
 
-  std::vector<dlib::full_object_detection> landmarksAll;
-
   for (const auto& face : faceRects) {
     dlib::full_object_detection landmarks = landmarkDetector(dlibImage, face);
     std::cout << "Landmarks: " << landmarks.num_parts() << std::endl;
-    renderFace(image, landmarks);
-    cv::rectangle(image, cv::Point(face.left(), face.top()),
-                  cv::Point(face.right(), face.bottom()),
-                  cv::Scalar{0, 244, 0});
+    auto diff = 1.60 * (landmarks.part(45).x() - landmarks.part(36).x());
+    double p = double(diff) / glasses.cols;
+    std::cout << "Diff: " << diff << " p: " << p << std::endl;
+    cv::resize(glasses, glasses, cv::Size(diff, glasses.rows * p));
+    cv::Rect r(landmarks.part(36).x() - 100,
+               landmarks.part(36).y() - glasses.rows / 2, diff, glasses.rows);
+    cv::Mat roi = image(r);
+    cv::imshow("Roi", roi);
+    std::cout << "Roi: " << roi.size() << std::endl;
+    std::cout << "Glasses: " << glasses.size() << std::endl;
+
+    auto mDiff = landmarks.part(54).x() - landmarks.part(48).x();
+    auto mp = double(mDiff) / cigar.rows;
+    cv::resize(cigar, cigar, cv::Size(mDiff, cigar.rows * mp));
+    std::cout << "Cigar after: " << cigar.size() << std::endl;
+    cv::Rect rCigar(landmarks.part(66).x() - cigar.size().width,
+                    landmarks.part(66).y(), mDiff, cigar.rows);
+    auto cigarRoi = image(rCigar);
+
+    cv::Mat channels[3];
+    cv::Mat channelsGlasses[4];
+    cv::Mat channelsCigar[4];
+    cv::Mat channelsCigarRoi[3];
+    cv::split(roi, channels);
+    cv::split(glasses, channelsGlasses);
+    cv::split(cigar, channelsCigar);
+    cv::split(cigarRoi, channelsCigarRoi);
+
+    cv::circle(image, cv::Point(landmarks.part(48).x(), landmarks.part(48).y()),
+               5, cv::Scalar{0, 222, 0}, -1);
+    cv::circle(image, cv::Point(landmarks.part(54).x(), landmarks.part(54).y()),
+               5, cv::Scalar{0, 222, 0}, -1);
+    cv::circle(image, cv::Point(landmarks.part(66).x(), landmarks.part(66).y()),
+               5, cv::Scalar{0, 222, 0}, -1);
+
+    cv::Mat mask, cigarMask;
+    cv::bitwise_not(channelsGlasses[3], mask);
+    cv::imshow("mask", mask);
+
+    cv::bitwise_not(channelsCigar[3], cigarMask);
+    cv::imshow("cigar mask", cigarMask);
+
+    cv::imshow("glass ch", channelsGlasses[0]);
+
+    cv::bitwise_and(channels[0], mask, channels[0]);
+    cv::add(channels[0], channelsGlasses[0], channels[0]);
+    cv::bitwise_and(channels[1], mask, channels[1]);
+    cv::add(channels[1], channelsGlasses[0], channels[1]);
+    cv::bitwise_and(channels[2], mask, channels[2]);
+    cv::add(channels[2], channelsGlasses[0], channels[2]);
+
+    cv::Mat maskedEyes;
+    cv::merge(channels, 3, maskedEyes);
+
+    cv::bitwise_and(channelsCigarRoi[0], cigarMask, channelsCigarRoi[0]);
+    cv::add(channelsCigarRoi[0], channelsCigar[0], channelsCigarRoi[0]);
+    cv::bitwise_and(channelsCigarRoi[1], cigarMask, channelsCigarRoi[1]);
+    cv::add(channelsCigarRoi[1], channelsCigar[1], channelsCigarRoi[1]);
+    cv::bitwise_and(channelsCigarRoi[2], cigarMask, channelsCigarRoi[2]);
+    cv::add(channelsCigarRoi[2], channelsCigar[2], channelsCigarRoi[2]);
+
+    cv::Mat cigarFinal;
+    cv::merge(channelsCigarRoi, 3, cigarFinal);
+    cigarFinal.copyTo(cigarRoi);
+
+    cv::imshow("cigar roi first", channelsCigarRoi[0]);
+
+    cv::imshow("Cigar roi", cigarRoi);
+    cv::imshow("Roi", roi);
+    cv::imshow("maskedEyes", maskedEyes);
+    maskedEyes.copyTo(roi);
   }
 
   cv::imshow("Image", image);
+  cv::imshow("Glasses", glasses);
+  cv::imshow("Cigar", cigar);
   cv::waitKey();
   cv::destroyAllWindows();
   return 0;
